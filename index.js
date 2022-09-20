6208b74d3f531cc608cd0f03762d8a56856a4d50
Angela Pace <angpace13@gmail.com>
2022-09-20 13:26:27 -0400

    commit tree a6f0c01ba389ceb804e463eb64553bacea622cf5

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Angela Pace",
+    streetAddress: "29 Doreen Drive"
+}
+function updateEmployeeWithKeyAndValue(employee, streetAddress){
+    const newEmployee = { ...employee}
+        newEmployee[streetAddress] = "11 Broadway";
+    
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee){
+    employee.streetAddress = "12 Broadway";
+    return employee
+}
+
+function deleteFromEmployeeByKey(newEmployee, key){
+   const newerEmployee = { ...employee}
+    delete newerEmployee[key]
+
+    return newerEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+
+    return employee
+}
